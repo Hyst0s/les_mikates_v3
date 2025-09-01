b200a0f08e06afa192424a045fab3b62e619a646
Hyst0s <kr.richard44@gmail.com>
2025-09-01 15:32:07 +0400

    v3.06

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,4 @@
-  <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../includes/header.css">
-    <link rel="stylesheet" href="../includes/footer.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poly:ital@0;1&family=Passion+One:wght@400;700;900&display=swap" rel="stylesheet">
-    <title>Les mikatés de Betti</title>
-</head>
-  
+
   
   <footer>
   <div class="text4">
