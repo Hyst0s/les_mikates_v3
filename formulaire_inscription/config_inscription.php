f5b3e0f5d57ed019611a0a4dbb8db4f4e6c62e20
Hyst0s <kr.richard44@gmail.com>
2025-09-02 15:28:59 +0400

    v3.1.1 config inscription

--- formulaire_inscription/config_inscription.php
+++ formulaire_inscription/config_inscription.php
@@ -5,25 +5,25 @@ include '../bdd/connex_bdd.php';
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $email = $_POST['email'];
-$mot_de_passe = $_POST['mot de passe'];
-$confirm_mot_de_passe = $POST['confirm_mot_de_passe'];
+$mot_de_passe = $_POST['mdp'];
+$confirm_password = $_POST['mdp2'];
 
 
-if ($_POST['password'] !==$confirm_password) {
+if ($_POST['mdp'] !== $confirm_password) {
     echo "les mots de passe ne correspondent pas.";
     exit;
 }
 
-$sql = "INSERT INTO utilisateur (nom, prenom, email, pseudo, mot_de_passe)
-VALUES ('$nom', '$prenom', '$email', '$pseudo', '$mot_de_passe')";
+$sql = "INSERT INTO utilisateur (nom, prenom, email, mot_de_passe)
+VALUES ('$nom', '$prenom', '$email', '$mot_de_passe')";
 
-if ($conn->query($sql) === TRUE) {
+if ($connexion->query($sql) === TRUE) {
     echo "Inscription réussie. Vous pouvez maintenant vous connecter.";
    } else {
-    echo "Erreur: lors de l'inscription : " . $conn->error;
+    echo "Erreur: lors de l'inscription : " . $connexion->error();
    }
 
-   $conn->close();
+   $connexion->close();
    ?>
     
 
--- formulaire_inscription/inscription.php
+++ formulaire_inscription/inscription.php
@@ -3,7 +3,7 @@
 <main class="card" role="main" aria-labelledby="titre">
     <h1 id="titre" class="title">Inscription</h1>
 
-    <form action="#" method="post" novalidate>
+    <form action="../formulaire_inscription\config_inscription.php" method="post">
       <div class="field">
         <label for="nom">Nom</label>
         <input id="nom" name="nom" type="text" placeholder="Nom" required autocomplete="family-name">
@@ -21,12 +21,12 @@
 
       <div class="field">
         <label for="mdp">Mot de passe</label>
-        <input id="mdp" name="mdp" type="password" placeholder="Mot de passe" required autocomplete="new-password" minlength="8">
+        <input id="mdp" name="mdp" type="password" placeholder="Mot de passe" required autocomplete="new-password" minlength="5">
       </div>
 
       <div class="field">
         <label for="mdp2">Confirmer mot de passe</label>
-        <input id="mdp2" name="mdp2" type="password" placeholder="Confirmer mot de passe" required autocomplete="new-password" minlength="8">
+        <input id="mdp2" name="mdp2" type="password" placeholder="Confirmer mot de passe" required autocomplete="new-password" minlength="5">
       </div>
 
       <button type="submit">Inscription</button>
